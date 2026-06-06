a370429ee694bc51e1c4f340cc94ae05374317bc
Dinel <dinelve123@gmail.com>
2026-06-06 18:46:31 -0400

    fixed typo on length of Weyl set

--- src/quotonic/utils.py
+++ src/quotonic/utils.py
@@ -242,7 +242,7 @@ def genWeylSet(m: int = 2, vectorized: bool = False) -> np_ndarray:
         vectorized: if True, calculates the Weyl matrices using vectorized operations for improved performance.
         
     Returns:
-        A list of $m^2 - 1$ $m \\times m$ Weyl matrices
+        A list of $m^2$ $m \\times m$ Weyl matrices
 
     Examples:
         >>> genWeylSet()
@@ -271,7 +271,7 @@ def genWeylSet(m: int = 2, vectorized: bool = False) -> np_ndarray:
                 [[ 0.+0.0000000e+00j,  1.+0.0000000e+00j],
                 [-1.+1.2246468e-16j, -0.+0.0000000e+00j]]])
     """
-    
+
     if vectorized:
         return np.array([np.exp(2j * np.pi * m_ * np.arange(m) / m)[:, None] * np.roll(np.eye(m), n, axis=1) for m_ in range(m) for n in range(m)], dtype=complex)
     
